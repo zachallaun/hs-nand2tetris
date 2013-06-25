fba0a9830a79b2396c5f01ecaf242ccc970ee134
Laura Lindzey <lindzey@gmail.com>
2013-06-25 01:59:02 -0400

    adding built-in variables, and it now passes rect

--- 06/Assembler.py
+++ 06/Assembler.py
@@ -8,7 +8,12 @@ class hackAssembler():
         # Where we store the labels and variables
         # TODO: Actually, it might make more sense to have separate dicts?
         # @_label_ isn't followed immeidately by a jump ... 
-        self.vardict = {}
+        # initialize w/ the predefined symbols
+        self.vardict = {'SP':0, 'LCL':1, 'ARG':2, 'THIS':3, 'THAT':4, 
+                        'R0':0, 'R1':1, 'R2':2, 'R3':3, 'R4':4, 'R5':5,
+                        'R6':6, 'R7':7, 'R8':8, 'R9':9, 'R10':10, 'R11':11,
+                        'R12':12, 'R13':13, 'R14':14, 'R15':15, 
+                        'SCREEN':16384, 'KBD':24576}
         # if we encounter a label this is the line that it'll refer to
         #next_line = 1
         # if we encounter a variable name, this is the address it'll get
