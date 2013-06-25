bee3dfd2ebb0aa7fccb75e6841f70d3ebc08c34f
Laura Lindzey <lindzey@gmail.com>
2013-06-25 01:11:57 -0400

    assembler is now a class, so it can have state (needed for handling the variables)

--- 06/Assembler.py
+++ 06/Assembler.py
@@ -2,192 +2,185 @@
 
 import re
 
-infile = open('input.asm', 'r')
+class hackAssembler():
 
-# In hack's assembly language, @# indicates that the 
-# input number should be written to the A register
-# The equivalent machine language representation is
-# '0' then 15 digits of # in binary (ignoring overflow)
-#
-# Symbols may be used in place of the '#'
-#
-def handle_a_expr(line):
-    # I could error if it has any non-allowed symbold in the variable
-    # name, but I'm avoiding that for now
-    # "a user-defined symbol may be any combination of letters, digits, 
-    # underscore (_), dot (.), dollar sign ($), and colon (:) 
-    # that does not begin with a digit." (from Ch6)
-    # re.search("\+|\-|\*|/|&|\||!|@", line[1:])
-    # it's a hard-coded constant (all digits!), 
-    # so no need to deal with variable names
-    if line[1:].isdigit():
-        decnum = int(line[1:])
-        binnum = bin(decnum)[2:]
-        # checking bounds!
-        if (0 > decnum) or (decnum >= 2**15):
-            raise Exception("Assembler: out-of-range assignment to A!:  " + line)
-        numdigits = min(15, len(binnum))
-        numzeros = 16-numdigits
-        bincmd='0'*numzeros + binnum[-numdigits:]
-    # TODO: check here for '(' which is the start of labels 
-    #(which don't increment the line #)
-    else: 
-        # TODO: better way to set allowed symbols/characters?
-        if line[1].isdigit() or line[1] in ['-', '*', '+', '/', '&', '|', '!']:
-            raise Exception("Assembler: illegal variable name:  " + line)
-        print "Assembler: Variables NYI!: " + line
-    return [bincmd]
+    def __init__(self):
+        # Where we store the labels and variables
+        # TODO: Actually, it might make more sense to have separate dicts?
+        # @_label_ isn't followed immeidately by a jump ... 
+        vardict = {}
+        # if we encounter a label this is the line that it'll refer to
+        next_line = 1
+        # if we encounter a variable name, this is the address it'll get
+        next_addr = 16
 
-# Questions:
-# * Can multiple registers be set as the output of an assignment?
-#    e.g.: A,D=D+1. => Yup! MD=D+1
-#
-# I'm building this up incrementally, starting w/ single 
+    def handle_a_expr(self, line):
+        # I could error if it has any non-allowed symbold in the variable
+        # name, but I'm avoiding that for now
+        # "a user-defined symbol may be any combination of letters, digits, 
+        # underscore (_), dot (.), dollar sign ($), and colon (:) 
+        # that does not begin with a digit." (from Ch6)
+        # re.search("\+|\-|\*|/|&|\||!|@", line[1:])
+        # it's a hard-coded constant (all digits!), 
+        # so no need to deal with variable names
+        if line[1:].isdigit():
+            decnum = int(line[1:])
+            binnum = bin(decnum)[2:]
+            # checking bounds!
+            if (0 > decnum) or (decnum >= 2**15):
+                raise Exception("Assembler: out-of-range assignment to A!:  " + line)
+            numdigits = min(15, len(binnum))
+            numzeros = 16-numdigits
+            bincmd='0'*numzeros + binnum[-numdigits:]
+        # TODO: check here for '(' which is the start of labels 
+        #(which don't increment the line #)
+        else: 
+            # TODO: better way to set allowed symbols/characters?
+            if line[1].isdigit() or line[1] in ['-', '*', '+', '/', '&', '|', '!']:
+                raise Exception("Assembler: illegal variable name:  " + line)
+            print "Assembler: Variables NYI!: " + line
+        return [bincmd]
 
-# NB - format of a C expr is
-# x=y;JMP
-def segment_c_expr(line):
-    # if line contains '=', figure out what registers to assign to; otherwise, 
-    if '=' in line:
-        tokens = line.split('=')
-        dest = tokens[0]
-        line = tokens[1]
-    else:
-        dest = None
-    if ';' in line:
-        tokens = line.split(';')
-        expr = tokens[0]
-        jump = tokens[1]
-    else:
-        expr = line
-        jump = None
-    return (dest, expr, jump)
-
-def parse_dest(dest):
-    # TODO: Ignores poorly-formed dest commands
-    if dest is None:
-        destcmd = '000'
-    else:
-        destcmd = ''
-        if 'A' in dest:
-            destcmd = destcmd + '1'
+    # NB - format of a C expr is
+    # x=y;JMP
+    def segment_c_expr(self, line):
+        if '=' in line:
+            tokens = line.split('=')
+            dest = tokens[0]
+            line = tokens[1]
         else:
-            destcmd = destcmd + '0'
-        if 'D' in dest:
-            destcmd = destcmd + '1'
+            dest = None
+        if ';' in line:
+            tokens = line.split(';')
+            expr = tokens[0]
+            jump = tokens[1]
         else:
-            destcmd = destcmd + '0'
-        if 'M' in dest:
-            destcmd = destcmd + '1'
-        else:
-            destcmd = destcmd + '0'
-    return destcmd
+            expr = line
+            jump = None
+        return (dest, expr, jump)
 
-# parses jump command - raises Exception if bad command
-def parse_jump(jump):
-    if jump is None:
-        jumpcmd = '000'
-    elif jump == 'JGT':
-        jumpcmd = '001'
-    elif jump == 'JEQ':
-        jumpcmd = '010'
-    elif jump == 'JGE':
-        jumpcmd = '011'
-    elif jump == 'JLT':
-        jumpcmd = '100'
-    elif jump == 'JNE':
-        jumpcmd = '101'
-    elif jump == 'JLE':
-        jumpcmd = '110'
-    elif jump == 'JMP':
-        jumpcmd = '111'
-    else:
-        raise Exception("Assembler: Illegal jump command: " + jump)
-    return jumpcmd
+    def parse_dest(self, dest):
+        # TODO: Ignores poorly-formed dest commands
+        if dest is None:
+            destcmd = '000'
+        else:
+            destcmd = ''
+            if 'A' in dest:
+                destcmd = destcmd + '1'
+            else:
+                destcmd = destcmd + '0'
+            if 'D' in dest:
+                destcmd = destcmd + '1'
+            else:
+                destcmd = destcmd + '0'
+            if 'M' in dest:
+                destcmd = destcmd + '1'
+            else:
+                destcmd = destcmd + '0'
+        return destcmd
+
+    # parses jump command - raises Exception if bad command
+    def parse_jump(self, jump):
+        if jump is None:
+            jumpcmd = '000'
+        elif jump == 'JGT':
+            jumpcmd = '001'
+        elif jump == 'JEQ':
+            jumpcmd = '010'
+        elif jump == 'JGE':
+            jumpcmd = '011'
+        elif jump == 'JLT':
+            jumpcmd = '100'
+        elif jump == 'JNE':
+            jumpcmd = '101'
+        elif jump == 'JLE':
+            jumpcmd = '110'
+        elif jump == 'JMP':
+            jumpcmd = '111'
+        else:
+            raise Exception("Assembler: Illegal jump command: " + jump)
+        return jumpcmd
     
-# there are a limited number of allowed calculations ...
-# so, effectively do a switch on them. This feels dirty.
-def parse_calc(calc):
-    if calc == '0':
-        calccmd = '0101010'
-    elif calc == '1':
-        calccmd = '0111111'
-    elif calc == '-1':
-        calccmd = '0111010'
-    elif calc == 'D':
-        calccmd = '0001100'
-    elif calc == 'A':
-        calccmd = '0110000'
-    elif calc == '!D':
-        calccmd = '0001101'
-    elif calc == '!A':
-        calccmd = '0110001'
-    elif calc == '-D':
-        calccmd = '0001111'
-    elif calc == '-A':
-        calccmd = '0110011'
-    elif calc == 'D+1' or calc == '1+D':
-        calccmd = '0011111'
-    elif calc == 'A+1' or calc == '1+A':
-        calccmd = '0110111'
-    elif calc == 'D-1':
-        calccmd = '0001110'
-    elif calc == 'A-1':
-        calccmd = '0110010'
-    elif calc == 'D+A' or calc == 'A+D':
-        calccmd = '0000010'
-    elif calc == 'D-A':
-        calccmd = '0010011'
-    elif calc == 'A-D':
-        calccmd = '0000111'
-    elif calc == 'D&A' or calc == 'A&D':
-        calccmd = '0000000'
-    elif calc == 'D|A' or calc == 'A|D':
-        calccmd = '0010101'
-    elif calc == 'M':
-        calccmd = '1110000'
-    elif calc == '!M':
-        calccmd = '1110001'
-    elif calc == '-M':
-        calccmd = '1110011'
-    elif calc == 'M+1' or calc == '1+M':
-        calccmd = '1110111'
-    elif calc == 'M-1':
-        calccmd = '1110010'
-    elif calc == 'D+M' or calc == 'M+D':
-        calccmd = '1000010'
-    elif calc == 'D-M':
-        calccmd = '1010011'
-    elif calc == 'M-D':
-        calccmd = '1000111'
-    elif calc == 'D&M' or calc == 'M&D':
-        calccmd = '1000000'
-    elif calc == 'D|M' or calc == 'M|D':
-        calccmd = '1010101'
-    else:
-        raise Exception("Assembler: illegal calculation: " + calc)
-        
-    return calccmd
+    # there are a limited number of allowed calculations ...
+    # so, effectively do a switch on them. This feels dirty.
+    def parse_calc(self, calc):
+        if calc == '0':
+            calccmd = '0101010'
+        elif calc == '1':
+            calccmd = '0111111'
+        elif calc == '-1':
+            calccmd = '0111010'
+        elif calc == 'D':
+            calccmd = '0001100'
+        elif calc == 'A':
+            calccmd = '0110000'
+        elif calc == '!D':
+            calccmd = '0001101'
+        elif calc == '!A':
+            calccmd = '0110001'
+        elif calc == '-D':
+            calccmd = '0001111'
+        elif calc == '-A':
+            calccmd = '0110011'
+        elif calc == 'D+1' or calc == '1+D':
+            calccmd = '0011111'
+        elif calc == 'A+1' or calc == '1+A':
+            calccmd = '0110111'
+        elif calc == 'D-1':
+            calccmd = '0001110'
+        elif calc == 'A-1':
+            calccmd = '0110010'
+        elif calc == 'D+A' or calc == 'A+D':
+            calccmd = '0000010'
+        elif calc == 'D-A':
+            calccmd = '0010011'
+        elif calc == 'A-D':
+            calccmd = '0000111'
+        elif calc == 'D&A' or calc == 'A&D':
+            calccmd = '0000000'
+        elif calc == 'D|A' or calc == 'A|D':
+            calccmd = '0010101'
+        elif calc == 'M':
+            calccmd = '1110000'
+        elif calc == '!M':
+            calccmd = '1110001'
+        elif calc == '-M':
+            calccmd = '1110011'
+        elif calc == 'M+1' or calc == '1+M':
+            calccmd = '1110111'
+        elif calc == 'M-1':
+            calccmd = '1110010'
+        elif calc == 'D+M' or calc == 'M+D':
+            calccmd = '1000010'
+        elif calc == 'D-M':
+            calccmd = '1010011'
+        elif calc == 'M-D':
+            calccmd = '1000111'
+        elif calc == 'D&M' or calc == 'M&D':
+            calccmd = '1000000'
+        elif calc == 'D|M' or calc == 'M|D':
+            calccmd = '1010101'
+        else:
+            raise Exception("Assembler: illegal calculation: " + calc)
+        return calccmd
 
-def handle_c_expr(line):
-    dest, calc, jump = segment_c_expr(line)
-    if calc is None:
-        raise Exception("Assembler: c-command must have command: " + line)
-    if calc == line:
-        raise Excpetion("Assembler: c-command line must contain '=' or ';' ... " + line)
+    def handle_c_expr(self, line):
+        dest, calc, jump = self.segment_c_expr(line)
+        if calc is None:
+            raise Exception("Assembler: c-command must have command: " + line)
+        if calc == line:
+            raise Excpetion("Assembler: c-command line must contain '=' or ';' ... " + line)
 
-    destcmd = parse_dest(dest)
-    jumpcmd = parse_jump(jump)
-    calccmd = parse_calc(calc)
+        destcmd = self.parse_dest(dest)
+        jumpcmd = self.parse_jump(jump)
+        calccmd = self.parse_calc(calc)
     
-    bincmd = '111' + calccmd + destcmd + jumpcmd
-    return [bincmd]
+        bincmd = '111' + calccmd + destcmd + jumpcmd
+        return [bincmd]
  
-
-
-
-if __name__ == "__main__":
-
+# preprocessing, not part of the assembler itself
+def clean_file(filename):
+    infile = open(filename, 'r')
     # First, I'm trying to strip out all whitespace and comments
     cleanlines = []
     for line in infile:
@@ -196,22 +189,35 @@ if __name__ == "__main__":
         if cleaned != '':
             cleanlines.append(cleaned)
     infile.close()
-    print cleanlines
+    return cleanlines
+
+
+if __name__== "__main__":
+
+    cleanlines = clean_file('test.asm')
+
+    # I'm doing this in two passes - the first to get all labels assigned,
+    # the second to properly encode all the asm->hack commands
+    # NB - unless I misunderstand how this is supposed to work, @label will
+    # have really weird behavior if it's not followed by 0;JMP
+    myassembler = hackAssembler()
+
 
     # next step is handling all of the A-commands
     print("\n\n  Assembling:\n")
-    outfile = open('input.hack', 'w');
+
     hackcode = []
     for line in cleanlines:
         print line
         if line[0] == '@':
-            cmd = handle_a_expr(line)
+            cmd = myassembler.handle_a_expr(line)
             hackcode.append(cmd)
         else:
-            cmd = handle_c_expr(line)
+            cmd = myassembler.handle_c_expr(line)
             hackcode.append(cmd)
-
         print cmd
+
+    outfile = open('test.hack', 'w');
     for cmd in hackcode:
         outfile.write(cmd[0] + '\n')
     outfile.close()
--- 06/test_assembler.py
+++ 06/test_assembler.py
@@ -4,6 +4,9 @@ import unittest
 
 import Assembler
 class TestCExpressions(unittest.TestCase):
+    def setUp(self):
+        self.asm = Assembler.hackAssembler()
+    
     def testIllegalFormat(self):
         in1 = 'JMP' # need to have somethign to left of ';'
         in2 = ';JMP' # need to have somethign to left of ';'
@@ -15,156 +18,156 @@ class TestCExpressions(unittest.TestCase):
 
     def testIllegalJump(self):
         in1 = 'D;'
-        self.assertRaises(Exception, Assembler.handle_c_expr, in1)
+        self.assertRaises(Exception, self.asm.handle_c_expr, in1)
         in2 = 'D;JJJ'
-        self.assertRaises(Exception, Assembler.handle_c_expr, in2)
+        self.assertRaises(Exception, self.asm.handle_c_expr, in2)
 
     def testDestParse(self):
         # NB - as it stands, silently ignores dest commands
         # that contain at least one 'A', 'D' or 'M' but are
         # otherwise ill-formed
         dest1 = None
-        out1 = Assembler.parse_dest(dest1)
+        out1 = self.asm.parse_dest(dest1)
         self.assertEqual('000', out1)
         dest2 = 'A'
-        out2 = Assembler.parse_dest(dest2)
+        out2 = self.asm.parse_dest(dest2)
         self.assertEqual('100', out2)
         dest3 = 'D'
-        out3 = Assembler.parse_dest(dest3)
+        out3 = self.asm.parse_dest(dest3)
         self.assertEqual('010', out3)
         dest4 = 'M'
-        out4 = Assembler.parse_dest(dest4)
+        out4 = self.asm.parse_dest(dest4)
         self.assertEqual('001', out4)
         dest5 = 'AM'
-        out5 = Assembler.parse_dest(dest5)
+        out5 = self.asm.parse_dest(dest5)
         self.assertEqual('101', out5)
         dest6 = 'AD'
-        out6 = Assembler.parse_dest(dest6)
+        out6 = self.asm.parse_dest(dest6)
         self.assertEqual('110', out6)
         dest7 = 'AMD'
-        out7 = Assembler.parse_dest(dest7)
+        out7 = self.asm.parse_dest(dest7)
         self.assertEqual('111', out7)
         dest8 = 'DM'
-        out8 = Assembler.parse_dest(dest8)
+        out8 = self.asm.parse_dest(dest8)
         self.assertEqual('011', out8)
 
     def testJumpParse(self):
         # nominal jump commands
         cmd1 = None
-        out1 = Assembler.parse_jump(cmd1)
+        out1 = self.asm.parse_jump(cmd1)
         self.assertEqual('000', out1)
         cmd2 = 'JGT'
-        out2 = Assembler.parse_jump(cmd2)
+        out2 = self.asm.parse_jump(cmd2)
         self.assertEqual('001', out2)
         cmd3 = 'JEQ'
-        out3 = Assembler.parse_jump(cmd3)
+        out3 = self.asm.parse_jump(cmd3)
         self.assertEqual('010', out3)
         cmd4 = 'JGE'
-        out4 = Assembler.parse_jump(cmd4)
+        out4 = self.asm.parse_jump(cmd4)
         self.assertEqual('011', out4)
         cmd5 = 'JLT'
-        out5 = Assembler.parse_jump(cmd5)
+        out5 = self.asm.parse_jump(cmd5)
         self.assertEqual('100', out5)
         cmd6 = 'JNE'
-        out6 = Assembler.parse_jump(cmd6)
+        out6 = self.asm.parse_jump(cmd6)
         self.assertEqual('101', out6)
         cmd7 = 'JLE'
-        out7 = Assembler.parse_jump(cmd7)
+        out7 = self.asm.parse_jump(cmd7)
         self.assertEqual('110', out7)
         cmd8 = 'JMP'
-        out8 = Assembler.parse_jump(cmd8)
+        out8 = self.asm.parse_jump(cmd8)
         self.assertEqual('111', out8)
         
         # now for the illegal expressions...
         cmd9 = 'JMP2'
-        self.assertRaises(Exception, Assembler.parse_jump, cmd9)
+        self.assertRaises(Exception, self.asm.parse_jump, cmd9)
         cmd10 = 'asdf'
-        self.assertRaises(Exception, Assembler.parse_jump, cmd10)
+        self.assertRaises(Exception, self.asm.parse_jump, cmd10)
 
     def testCalcParse(self):
         #nominal calc commands
         clc1 = '1'
-        out1 = Assembler.parse_calc(clc1)
+        out1 = self.asm.parse_calc(clc1)
         self.assertEqual('0111111', out1)
         clc2 = '0'
-        out2 = Assembler.parse_calc(clc2)
+        out2 = self.asm.parse_calc(clc2)
         self.assertEqual('0101010', out2)
         clc3 = 'M'
-        out3 = Assembler.parse_calc(clc3)
+        out3 = self.asm.parse_calc(clc3)
         self.assertEqual('1110000', out3)
         clc4 = 'A'
-        out4 = Assembler.parse_calc(clc4)
+        out4 = self.asm.parse_calc(clc4)
         self.assertEqual('0110000', out4)
         clc5 = 'D'
-        out5 = Assembler.parse_calc(clc5)
+        out5 = self.asm.parse_calc(clc5)
         self.assertEqual('0001100', out5)
         clc6 = 'A|D'
-        out6 = Assembler.parse_calc(clc6)
+        out6 = self.asm.parse_calc(clc6)
         self.assertEqual('0010101', out6)
         clc7 = 'A&D'
-        out7 = Assembler.parse_calc(clc7)
+        out7 = self.asm.parse_calc(clc7)
         self.assertEqual('0000000', out7)
         clc8 = 'M-1'
-        out8 = Assembler.parse_calc(clc8)
+        out8 = self.asm.parse_calc(clc8)
         self.assertEqual('1110010', out8)
         clc9 = 'D-M'
-        out9 = Assembler.parse_calc(clc9)
+        out9 = self.asm.parse_calc(clc9)
         self.assertEqual('1010011', out9)
         clc10 = 'M&D'
-        out10 = Assembler.parse_calc(clc10)
+        out10 = self.asm.parse_calc(clc10)
         self.assertEqual('1000000', out10)
 
         # bad ones ...
         bad1 = 'A+M'
-        self.assertRaises(Exception, Assembler.parse_calc, bad1)
+        self.assertRaises(Exception, self.asm.parse_calc, bad1)
         bad2 = 'A+M+D'
-        self.assertRaises(Exception, Assembler.parse_calc, bad2)
+        self.assertRaises(Exception, self.asm.parse_calc, bad2)
         bad3 = '1+1'
-        self.assertRaises(Exception, Assembler.parse_calc, bad3)
+        self.assertRaises(Exception, self.asm.parse_calc, bad3)
 
     # This is probably overkill ... 
     def testJumpCommand(self):
         # start with the good ones
         in0 = 'D=0'
-        out0 = Assembler.handle_c_expr(in0)
-        self.assertEqual(out0, ['0000101010010000'])
+        out0 = self.asm.handle_c_expr(in0)
+        self.assertEqual(out0, ['1110101010010000'])
         in1 = 'D;JGT'
-        out1 = Assembler.handle_c_expr(in1)
-        self.assertEqual(out1, ['0000001100000001'])
+        out1 = self.asm.handle_c_expr(in1)
+        self.assertEqual(out1, ['1110001100000001'])
         in2 = 'D;JEQ'
-        out2 = Assembler.handle_c_expr(in2)
-        self.assertEqual(out2, ['0000001100000010'])
+        out2 = self.asm.handle_c_expr(in2)
+        self.assertEqual(out2, ['1110001100000010'])
         in3 = 'D;JGE'
-        out3 = Assembler.handle_c_expr(in3)
-        self.assertEqual(out3, ['0000001100000011'])
+        out3 = self.asm.handle_c_expr(in3)
+        self.assertEqual(out3, ['1110001100000011'])
         in4 = 'D;JLT'
-        out4 = Assembler.handle_c_expr(in4)
-        self.assertEqual(out4, ['0000001100000100'])
+        out4 = self.asm.handle_c_expr(in4)
+        self.assertEqual(out4, ['1110001100000100'])
         in5 = 'D;JNE'
-        out5 = Assembler.handle_c_expr(in5)
-        self.assertEqual(out5, ['0000001100000101'])
+        out5 = self.asm.handle_c_expr(in5)
+        self.assertEqual(out5, ['1110001100000101'])
         in6 = 'D;JLE'
-        out6 = Assembler.handle_c_expr(in6)
-        self.assertEqual(out6, ['0000001100000110'])
+        out6 = self.asm.handle_c_expr(in6)
+        self.assertEqual(out6, ['1110001100000110'])
         in7 = 'D;JMP'
-        out7 = Assembler.handle_c_expr(in7)
-        self.assertEqual(out7, ['0000001100000111'])
+        out7 = self.asm.handle_c_expr(in7)
+        self.assertEqual(out7, ['1110001100000111'])
             
         
     # as a first step, I'm going to only parse assignments
     def testZeroAssignment(self):
         in1 = 'A=0'
-        out1 = Assembler.handle_c_expr(in1)
-        self.assertEqual(out1, ['0000101010100000'])
+        out1 = self.asm.handle_c_expr(in1)
+        self.assertEqual(out1, ['1110101010100000'])
         in2 = 'D=0'
-        out2 = Assembler.handle_c_expr(in2)
-        self.assertEqual(out2, ['0000101010010000'])
+        out2 = self.asm.handle_c_expr(in2)
+        self.assertEqual(out2, ['1110101010010000'])
         in3 = 'M=0'
-        out3 = Assembler.handle_c_expr(in3)
-        self.assertEqual(out3, ['0000101010001000'])
+        out3 = self.asm.handle_c_expr(in3)
+        self.assertEqual(out3, ['1110101010001000'])
         in4 = 'ADM=0'
-        out4 = Assembler.handle_c_expr(in4)
-        self.assertEqual(out4, ['0000101010111000'])
+        out4 = self.asm.handle_c_expr(in4)
+        self.assertEqual(out4, ['1110101010111000'])
         
 # these are OK
 # '=0;JMP' # computes 0, JMP
@@ -174,34 +177,37 @@ class TestCExpressions(unittest.TestCase):
 
 # this tests A-epxressions without any vars
 class TestAExpression(unittest.TestCase):
+    def setUp(self):
+        self.asm = Assembler.hackAssembler()
+
     def testNominal(self):
         in1 = '@2'
-        out1 = Assembler.handle_a_expr(in1)
+        out1 = self.asm.handle_a_expr(in1)
         self.assertEqual(out1, ['0000000000000010'])
 
         in2 = '@3'
-        out2 = Assembler.handle_a_expr(in2)
+        out2 = self.asm.handle_a_expr(in2)
         self.assertEqual(out2, ['0000000000000011'])
 
         in2 = '@20000'
-        out2 = Assembler.handle_a_expr(in2)
+        out2 = self.asm.handle_a_expr(in2)
         self.assertEqual(out2, ['0100111000100000'])
 
     # The provided assembler doesn't warn about this...
     def testOverflow(self):
         in1 = '@32768'
-        self.assertRaises(Exception, Assembler.handle_a_expr, in1)
+        self.assertRaises(Exception, self.asm.handle_a_expr, in1)
 
     def testVarNames(self):
         in1 = '@0f1'
-        self.assertRaises(Exception, Assembler.handle_a_expr, in1)
-
+        self.assertRaises(Exception, self.asm.handle_a_expr, in1)
         in2 = '@-1'
-        self.assertRaises(Exception, Assembler.handle_a_expr, in2)
+        self.assertRaises(Exception, self.asm.handle_a_expr, in2)
+
         
 # add A expressions with vars? 
 # test this by defining variables in sequence, then checking their addresses
-# will require turning the Assembler into a class s.t. it can keep track of  
+# will require turning the self.asm into a class s.t. it can keep track of  
 # that state. 
 # in1 = '@f00' # this one is ok
 # in2 = '@foo'
